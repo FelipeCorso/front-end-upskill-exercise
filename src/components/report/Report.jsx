3b7c6329ae68c0cd660de1c4d98f1277985ccfba
Felipe Corso <felipe.corso@dunelm.com>
2023-03-07 21:27:43 +0000

    Report component

---
+++ src/components/report/Report.jsx
@@ -0,0 +1,15 @@
+import ReportLine from "../report-line/ReportLine";
+
+const Report = ({ items }) => {
+  console.log(items);
+
+  return (
+    <ul>
+      {items.map((item, index) => (
+        <ReportLine item={item} key={index} />
+      ))}
+    </ul>
+  );
+};
+
+export default Report;
